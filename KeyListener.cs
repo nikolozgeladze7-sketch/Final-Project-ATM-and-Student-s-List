2a9b7a2153d4e40882739425d1be6be920d0eaff
NikolozGeladze <nikolozgeladze7@gmail.com>
2025-11-09 15:16:16 +0400

    Add files via upload

---
+++ KeyListener.cs
@@ -0,0 +1,52 @@
+﻿internal class KeyListener
+{
+    public event Action OnBalanceCheck = null!;
+    public event Action<decimal> OnWithdraw = null!;
+    public event Action<decimal> OnAddMoney = null!;
+
+    public void StartListening()
+    {
+        decimal amount;
+
+        while (true)
+        {
+            ConsoleKeyInfo keyValue = InsertKey();
+
+            if (keyValue.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+            switch (keyValue.Key)
+            {
+                case ConsoleKey.W:
+                    Console.Write("Enter amount: ");
+                    amount = Convert.ToDecimal(Console.ReadLine());
+                    OnWithdraw?.Invoke(amount);
+                    break;
+                case ConsoleKey.A:
+                    Console.Write("Enter amount: ");
+                    amount = Convert.ToDecimal(Console.ReadLine());
+                    OnAddMoney?.Invoke(amount);
+                    break;
+                case ConsoleKey.B:
+                    OnBalanceCheck?.Invoke();
+                    break;
+                default:
+                    Console.WriteLine("Invalid key. Try again!");
+                    break;
+            }
+        }
+    }
+
+    private static ConsoleKeyInfo InsertKey()
+    {
+        Console.WriteLine("Enter A - to add money;");
+        Console.WriteLine("Enter B - to see balance;");
+        Console.WriteLine("Enter W - to withdraw money;");
+        Console.WriteLine("Esc - to exit:");
+
+        ConsoleKeyInfo keyValue = Console.ReadKey(true);
+        return keyValue;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿BankingSystem bankingSystem = new BankingSystem();
+KeyListener keyListener = new KeyListener();
+
+keyListener.OnBalanceCheck += bankingSystem.ShowBalance;
+keyListener.OnWithdraw += bankingSystem.WithdrawMoney;
+keyListener.OnAddMoney += bankingSystem.AddMoney;
+
+keyListener.StartListening();
+
+
+
+
+
+
+
+
+
+
+//StudentManager studentManager = new StudentManager();
+//Student student = new Student();
+
+//studentManager.OnAddStudent += student.AddStudent;
+//studentManager.OnRemoveStudent += student.RemoveStudent;
+//studentManager.OnShowStudent += student.ShowAll;
+//studentManager.OnSearchStudent += student.Search;
+
+//studentManager.StartStudentList();
